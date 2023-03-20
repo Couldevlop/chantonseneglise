c7cdb87451a982cf6a4d78b7c8fb3b26b14053a3
couldevlop <coulwao@gmail.com>
2023-03-20 00:43:30 +0000

    first commit

---
+++ src/test/java/com/openlab/chantonseneglise/ChantonsenegliseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openlab.chantonseneglise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChantonsenegliseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
